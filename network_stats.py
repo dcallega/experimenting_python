e820fc6bb39b0aa7f47f5292f23ef37eeda2e312
Davide <dcallega@uci.edu>
2019-10-28 11:56:05 -0700

    Running and reporting correct statistics (visual test with 1 interface UP)

--- network_stats.py
+++ network_stats.py
@@ -2,6 +2,7 @@ import os
 import time
 from typing import List, Dict
 import numpy as np
+from collections import defaultdict
 
 '''
 time,IP_outgoing_pkts_dropped,TCP_num_fast_retransmission,
@@ -26,6 +27,21 @@ def test_time_read_write():
   print(time.time() - start_time)
 
 def parse_netstat_i(interfaces: List[str] = None) -> Dict[str, Dict[str, str]]:
+  """
+  Dictionary contains:
+  - for each interface
+    - Iface : Interface name
+      MTU   : Maximum Transmission Unit
+      RX-OK : Reciving ok [bytes]
+      RX-ERR: 
+      RX-DRP:
+      RX-OVR:
+      TX-OK : 
+      TX-ERR:
+      TX-DRP:
+      TX-OVR:
+      Flg   : State of connection
+  """
   out = os.popen('netstat -i').read()
   out = out.split("\n")
   lines = [e for e in out]
@@ -53,14 +69,16 @@ def parse_wireless(interfaces: List[str] = None) -> Dict[str, Dict[str, str]]:
   for e in lines[2:]:
     if len(e) > 0:
       tmp = e.split()
-      if interfaces is None or tmp[0][:5] in ifaces:
+      print(tmp)
+      interface = tmp[0].split(':')[0][:5]
+      tmp[0] = interface
+      if interfaces is None or interface in ifaces:
         tmp_ret = {header[i]: tmp[i] for i in range(len(header))}
-        ret[tmp[0]] = tmp_ret 
+        ret[interface] = tmp_ret
   return ret
 
 def ss_info_tcp():
   """
-
   Returns a dictionary with information about the available tcp connections.
   Info about fields: http://man7.org/linux/man-pages/man8/ss.8.html
   wscale: window scale scale factor for send,rcv
@@ -97,7 +115,7 @@ def ss_info_tcp():
     elif i%2==1 and conn is not None:
       tmp = out[i].strip().split()
       tmp1 = [e.split(':') for e in tmp]
-      d = {}
+      d = defaultdict(lambda : None)
       for e in tmp1:
         if len(e) > 1:
           d[e[0]] = e[1]
@@ -128,15 +146,11 @@ def iface_ip() -> Dict[str, str]:
     i += 1
   return ret
 
-
-if __name__=="__main__":
-  tmp = ss_info_tcp()
-  # [print(e) for e in tmp[sorted(tmp)[0]]]
+def test_timing():
   start = time.time()
-  TRIALS = 1
+  TRIALS = 10
   timings = []
   tmp = {}
-
   for _ in range(TRIALS):
     st = time.time()
     tmp = {}
@@ -145,7 +159,19 @@ if __name__=="__main__":
     tmp["ss"] = ss_info_tcp()
     tmp["iface_ip"] = iface_ip()
     timings.append(time.time()-st)
-  iface = 'wlp0s20f3'[:5]
+  print(len(header), len(final))
+  print(np.mean(timings), np.std(timings))
+  [print(e, tmp[e]) for e in tmp]
+
+if __name__=="__main__":
+  tcp_fields = ["wscale", "rto", "rtt", "ato", "mss", "pmtu", "mss", "cwnd", "bytes_acked", "bytes_received", "segs_out", "segs_in", "data_segs_out", "data_segs_in", "lastsnd", "lastrcv", "lastack", "busy", "rcv_rtt", "rcv_space", "rcv_ssthresh", "minrtt"]
+  tmp = {}
+  tmp["wireless"] = parse_wireless()
+  tmp["nstat_i"] = parse_netstat_i()
+  tmp["ss"] = ss_info_tcp()
+  tmp["iface_ip"] = iface_ip()
+  ifaces = list(tmp["wireless"])
+  iface = ifaces[0]
   final = []
   final.append(iface)
   header = ["Iface nick"]
@@ -158,25 +184,20 @@ if __name__=="__main__":
       if iface in e:
         [final.append(tmp[key][e][k]) for k in sorted(tmp[key][e])]
   rev_iface = {tmp["iface_ip"][e]: e[:5] for e in tmp["iface_ip"] if tmp['iface_ip'][e] is not None}
-  print(tmp['iface_ip'])
-  print(rev_iface)
   final_per_conn = []
   curr_header_len = len(header)
   for conn in tmp['ss']:
     if len(header) <= curr_header_len:
-      keys = sorted(tmp['ss'][conn])
-      header += sorted(tmp['ss'][conn])
+      keys = tcp_fields
+      header += tcp_fields
     ip = conn.split(':')[0]
     if ip in rev_iface:
       iface_of_ip = rev_iface[ip]
+      print(iface)
+      print(ip)
       for iface in tmp['ss']:
-        if iface in 
-      [final.append(tmp['ss'][e]) for e in keys]
+        if ip in iface:
+          [final.append(tmp['ss'][conn][e]) for e in tcp_fields]
     
-  print(header)
-  print(final)
+  [print(e) for e in zip(header, final)]
 
-  # print(len(header), len(final))
-
-  # print(np.mean(timings), np.std(timings))
-  # [print(e, tmp[e]) for e in tmp]
